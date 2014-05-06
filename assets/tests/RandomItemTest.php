aa188e6b203850829611b0d6c891b3d1aecd1c8c
Clay Branch <clay@movementstrategy.com>
2014-05-06 13:31:51 -0400

    added random value for random offset to tests

--- assets/tests/RandomItemTest.php
+++ assets/tests/RandomItemTest.php
@@ -275,6 +275,9 @@
 						// this value is going to be used to assign whether or not the 
 						// right random item is high or not 
 						6,
+						
+						// this is the value that will be used as the random offset
+						8,
 					),	
 				),
 				'extra_params' => array(
